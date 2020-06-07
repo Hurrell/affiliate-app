061fe7ba3aaf2b79fa8a7552621e070d7c37d79a
Hurrell <cem.hurrell@gmail.com>
2020-06-07 22:33:45 +0100

    Updates

---
+++ precache-manifest.9a81efc6b32c26346ff9a0374c318e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fece18a82c56386c8c09efb897abde5d",
+    "url": "/affiliate-app/index.html"
+  },
+  {
+    "revision": "06c04d1f37844e936a6d",
+    "url": "/affiliate-app/static/css/main.895cf745.chunk.css"
+  },
+  {
+    "revision": "1f4b017d2a157fb62403",
+    "url": "/affiliate-app/static/js/2.bcad6398.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/affiliate-app/static/js/2.bcad6398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c04d1f37844e936a6d",
+    "url": "/affiliate-app/static/js/main.e18fbf19.chunk.js"
+  },
+  {
+    "revision": "5ed11e2cc80bcaded652",
+    "url": "/affiliate-app/static/js/runtime-main.c708719d.js"
+  }
+]);
