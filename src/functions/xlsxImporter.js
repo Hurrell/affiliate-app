91468c2096d5d5beff817568226a68ef6b5f71d4
Hurrell <cem.hurrell@gmail.com>
2021-04-07 16:15:55 +0100

    Updated to import full sheet with tabs

--- src/functions/xlsxImporter.js
+++ src/functions/xlsxImporter.js
@@ -4,9 +4,14 @@ import XLSX from "xlsx";
 // const file = "../resources/exampleUSASales.xlsx";
 /* read a workbook */
 const arrayToJsObj = (data) => {
-  if (data[1].length !== 10) {
+  if (!data["Fee-Orders"]) {
+    throw Error("Sheet is not compatible: (No Fee-Orders Sheet)");
+  }
+
+  if (data["Fee-Orders"][1].length !== 10) {
     throw Error("Sheet is not compatible");
   }
+
   const feb2020Headers = [
     "Category",
     "Name",
@@ -21,15 +26,15 @@ const arrayToJsObj = (data) => {
   ];
 
   for (let i in feb2020Headers) {
-    if (feb2020Headers[i] !== data[1][i]) {
+    if (feb2020Headers[i] !== data["Fee-Orders"][1][i]) {
       throw Error("Data values not recognised");
     }
   }
 
-  let jsObjData = { name: data[0][0], data: [] };
+  let jsObjData = { name: data["Fee-Orders"][0][0], data: [] };
 
-  for (let i = 2; i < data.length; i++) {
-    let row = data[i];
+  for (let i = 2; i < data["Fee-Orders"].length; i++) {
+    let row = data["Fee-Orders"][i];
     jsObjData.data.push({
       category: row[0],
       name: row[1],
@@ -48,7 +53,9 @@ const arrayToJsObj = (data) => {
 };
 
 const xlsxImporter = (e) => {
+  //Convert an XLSX into jsonData
   let file = e.target.files[0];
+  let data = {};
   console.log("here is what the file looks like", e.target.files[0]);
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -57,10 +64,14 @@ const xlsxImporter = (e) => {
       // call 'xlsx' to read the file
       let workbook = XLSX.read(event.target.result, { type: "binary" });
       /* convert from workbook to array of arrays */
-      var first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      const data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+      workbook.SheetNames.forEach((sheetName) => {
+        let workSheet = workbook.Sheets[sheetName];
+        data[sheetName] = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+      });
+      // var first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      // const data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
 
-      // console.log(data);
+      console.log("data", data);
       resolve(arrayToJsObj(data));
     };
     reader.onerror = (e) => {
