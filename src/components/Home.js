eec4fa8be243a83a27e20127d43fd6ca74bafffa
Hurrell <cem.hurrell@gmail.com>
2021-04-13 15:37:27 +0100

    Home Page Stats

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,17 @@ const Home = ({
   selectedCategories,
   categories,
 }) => {
+  let currencyFormat = {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  };
+  let percentFormat = {
+    style: "percent",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  };
   return (
     <div className="home-column">
       <div>
@@ -18,29 +29,74 @@ const Home = ({
         <div className="home-row">
           <div>
             <h3>Items:</h3>
-            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+            <div className="stat">{basicStats.getOrdersVolume(jsObjData)}</div>
           </div>
           <div>
             <h3>Baskets:</h3>
-            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+            <div className="stat">
+              {basicStats.getTotalOrdersBaskets(jsObjData)}
+            </div>
           </div>
           <div>
             <h3>Average Item Cost:</h3>
             <div className="stat">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(Math.round(basicStats.getAverageItemCost(jsObjData)))}
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getAverageOrdersItemCost(jsObjData)
+              )}
             </div>
           </div>
           <div>
-            <h3>Sales Turnover:</h3>
+            <h3>Average Basket Cost:</h3>
             <div className="stat">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(
-                Math.round(basicStats.getTotalSalesTurnover(jsObjData))
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getAverageOrdersBasketCost(jsObjData)
+              )}
+            </div>
+          </div>
+          <div>
+            <h3>Turnover:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getTotalOrdersTurnover(jsObjData)
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Sales</h2>
+        <div className="home-row">
+          <div>
+            <h3>Items:</h3>
+            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+          </div>
+          <div>
+            <h3>Baskets:</h3>
+            <div className="stat">
+              {basicStats.getTotalSalesBaskets(jsObjData)}
+            </div>
+          </div>
+          <div>
+            <h3>Average Item Cost:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getAverageSalesItemCost(jsObjData)
+              )}
+            </div>
+          </div>
+          <div>
+            <h3>Average Basket Cost:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getAverageSalesBasketCost(jsObjData)
+              )}
+            </div>
+          </div>
+          <div>
+            <h3>Turnover:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getTotalSalesTurnover(jsObjData)
               )}
             </div>
           </div>
@@ -55,24 +111,32 @@ const Home = ({
           </div>
           <div>
             <h3>Baskets:</h3>
-            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+            <div className="stat">
+              {basicStats.getTotalReturnsBaskets(jsObjData)}
+            </div>
           </div>
           <div>
             <h3>Average Item Cost:</h3>
             <div className="stat">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(Math.round(basicStats.getAverageItemCost(jsObjData)))}
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                Math.round(basicStats.getAverageReturnedItemCost(jsObjData))
+              )}
+            </div>
+          </div>
+          <div>
+            <h3>Average Basket Cost:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                basicStats.getAverageReturnsBasketCost(jsObjData)
+              )}
             </div>
           </div>
           <div>
             <h3>Returns Turnover:</h3>
             <div className="stat">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(Math.round(basicStats.getTotalReturnsCost(jsObjData)))}
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                Math.round(basicStats.getTotalReturnsCost(jsObjData))
+              )}
             </div>
           </div>
         </div>
@@ -81,12 +145,28 @@ const Home = ({
         <h2>Fees</h2>
         <div className="home-row">
           <div>
-            <h3>Actual Income:</h3>
+            <h3>Average Fee Fraction:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", percentFormat).format(
+                basicStats.getAverageFeeFraction(jsObjData)
+              )}
+            </div>
+          </div>
+          <div>
+            <h3>Average Fee Per Item:</h3>
             <div className="stat">
               {new Intl.NumberFormat("en-US", {
                 style: "currency",
                 currency: "USD",
-              }).format(Math.round(basicStats.getTotalActualIncome(jsObjData)))}
+              }).format(basicStats.getAverageFee(jsObjData))}
+            </div>
+          </div>
+          <div>
+            <h3>Actual Income:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", currencyFormat).format(
+                Math.round(basicStats.getTotalActualIncome(jsObjData))
+              )}
             </div>
           </div>
         </div>
--- src/functions/basicStats.js
+++ src/functions/basicStats.js
@@ -1,7 +1,23 @@
 const basicStats = {};
 
+basicStats.getOrdersVolume = (data) => {
+  let items = 0;
+  for (let item of data["Fee-Orders"]) {
+    if (Number(item.qty)) {
+      items += Number(item.qty);
+    }
+  }
+  return items;
+};
+
 basicStats.getSalesVolume = (data) => {
-  return data["Fee-Orders"].length;
+  let sales = 0;
+  for (let item of data["Fee-Earnings"]) {
+    if (Number(item.itemsShipped)) {
+      sales += Number(item.itemsShipped);
+    }
+  }
+  return sales;
 };
 
 basicStats.getTotalReturns = (data) => {
@@ -24,11 +40,21 @@ basicStats.getTotalReturnsCost = (data) => {
   return returnsCost;
 };
 
-basicStats.getTotalSalesTurnover = (data) => {
+basicStats.getTotalOrdersTurnover = (data) => {
   let totalSales = 0;
   for (let item of data["Fee-Orders"]) {
     if (Number(item.qty)) {
       totalSales += Number(item.qty) * Number(item["price($)"]);
+    }
+  }
+  return totalSales;
+};
+
+basicStats.getTotalSalesTurnover = (data) => {
+  let totalSales = 0;
+  for (let item of data["Fee-Earnings"]) {
+    if (Number(item.itemsShipped)) {
+      totalSales += Number(item["revenue($)"]);
     }
   }
   return totalSales;
@@ -44,9 +70,101 @@ basicStats.getTotalActualIncome = (data) => {
   return actualIncome;
 };
 
-basicStats.getAverageItemCost = (data) => {
+basicStats.getAverageOrdersItemCost = (data) => {
+  return (
+    basicStats.getTotalOrdersTurnover(data) / basicStats.getOrdersVolume(data)
+  );
+};
+
+basicStats.getAverageSalesItemCost = (data) => {
   return (
     basicStats.getTotalSalesTurnover(data) / basicStats.getSalesVolume(data)
+  );
+};
+
+basicStats.getAverageReturnedItemCost = (data) => {
+  return (
+    basicStats.getTotalReturnsCost(data) / basicStats.getTotalReturns(data)
+  );
+};
+
+basicStats.getTotalOrdersBaskets = (data) => {
+  let baskets = 0;
+  data["Fee-Orders"].map((item, index) => {
+    if (Number(item.qty)) {
+      if (baskets == 0) {
+        baskets += 1;
+      } else if (item.date != data["Fee-Orders"][index - 1].date) {
+        baskets += 1;
+      }
+    }
+  });
+  return baskets;
+};
+
+basicStats.getTotalSalesBaskets = (data) => {
+  let baskets = 0;
+  data["Fee-Earnings"].map((item, index) => {
+    if (Number(item.itemsShipped)) {
+      if (baskets == 0) {
+        baskets += 1;
+      } else if (
+        item.dateShipped != data["Fee-Earnings"][index - 1].dateShipped
+      ) {
+        baskets += 1;
+      }
+    }
+  });
+  return baskets;
+};
+
+basicStats.getTotalReturnsBaskets = (data) => {
+  let baskets = 0;
+  data["Fee-Earnings"].map((item, index) => {
+    if (Number(item.returns)) {
+      if (baskets == 0) {
+        baskets += 1;
+      } else if (
+        item.dateShipped != data["Fee-Earnings"][index - 1].dateShipped
+      ) {
+        baskets += 1;
+      }
+    }
+  });
+  return baskets;
+};
+
+basicStats.getAverageOrdersBasketCost = (data) => {
+  return (
+    basicStats.getTotalOrdersTurnover(data) /
+    basicStats.getTotalOrdersBaskets(data)
+  );
+};
+
+basicStats.getAverageSalesBasketCost = (data) => {
+  return (
+    basicStats.getTotalSalesTurnover(data) /
+    basicStats.getTotalSalesBaskets(data)
+  );
+};
+
+basicStats.getAverageReturnsBasketCost = (data) => {
+  return (
+    basicStats.getTotalReturnsCost(data) /
+    basicStats.getTotalReturnsBaskets(data)
+  );
+};
+
+basicStats.getAverageFee = (data) => {
+  return (
+    basicStats.getTotalActualIncome(data) / basicStats.getSalesVolume(data)
+  );
+};
+
+basicStats.getAverageFeeFraction = (data) => {
+  return (
+    basicStats.getTotalActualIncome(data) /
+    basicStats.getTotalSalesTurnover(data)
   );
 };
 
