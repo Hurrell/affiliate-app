108375de2a1e0c8fc42ef57655f1b715db7432b6
Hurrell <cem.hurrell@gmail.com>
2021-04-13 12:30:21 +0100

    Home page updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import basicStats from "../functions/basicStats";
+
+import "./Home.css";
+
 const Home = ({
   chart,
   jsObjData,
@@ -9,9 +12,85 @@ const Home = ({
   categories,
 }) => {
   return (
-    <div>
-      Sales: {basicStats.getSalesVolume(jsObjData)}
-      Returns: {basicStats.getTotalReturns(jsObjData)}
+    <div className="home-column">
+      <div>
+        <h2>Orders</h2>
+        <div className="home-row">
+          <div>
+            <h3>Items:</h3>
+            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+          </div>
+          <div>
+            <h3>Baskets:</h3>
+            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+          </div>
+          <div>
+            <h3>Average Item Cost:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(Math.round(basicStats.getAverageItemCost(jsObjData)))}
+            </div>
+          </div>
+          <div>
+            <h3>Sales Turnover:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(
+                Math.round(basicStats.getTotalSalesTurnover(jsObjData))
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Returns</h2>
+        <div className="home-row">
+          <div>
+            <h3>Items:</h3>
+            <div className="stat">{basicStats.getTotalReturns(jsObjData)}</div>
+          </div>
+          <div>
+            <h3>Baskets:</h3>
+            <div className="stat">{basicStats.getSalesVolume(jsObjData)}</div>
+          </div>
+          <div>
+            <h3>Average Item Cost:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(Math.round(basicStats.getAverageItemCost(jsObjData)))}
+            </div>
+          </div>
+          <div>
+            <h3>Returns Turnover:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(Math.round(basicStats.getTotalReturnsCost(jsObjData)))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Fees</h2>
+        <div className="home-row">
+          <div>
+            <h3>Actual Income:</h3>
+            <div className="stat">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(Math.round(basicStats.getTotalActualIncome(jsObjData)))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/functions/basicStats.js
+++ src/functions/basicStats.js
@@ -14,8 +14,40 @@ basicStats.getTotalReturns = (data) => {
   return returns;
 };
 
-basicStats.getTotalSalesIncome = (data) => {
-  //   let totalSales = data;
+basicStats.getTotalReturnsCost = (data) => {
+  let returnsCost = 0;
+  for (let item of data["Fee-Earnings"]) {
+    if (Number(item.returns)) {
+      returnsCost += Number(item["revenue($)"]);
+    }
+  }
+  return returnsCost;
+};
+
+basicStats.getTotalSalesTurnover = (data) => {
+  let totalSales = 0;
+  for (let item of data["Fee-Orders"]) {
+    if (Number(item.qty)) {
+      totalSales += Number(item.qty) * Number(item["price($)"]);
+    }
+  }
+  return totalSales;
+};
+
+basicStats.getTotalActualIncome = (data) => {
+  let actualIncome = 0;
+  for (let item of data["Fee-Earnings"]) {
+    if (Number(item["adFees($)"])) {
+      actualIncome += Number(item["adFees($)"]);
+    }
+  }
+  return actualIncome;
+};
+
+basicStats.getAverageItemCost = (data) => {
+  return (
+    basicStats.getTotalSalesTurnover(data) / basicStats.getSalesVolume(data)
+  );
 };
 
 export default basicStats;
