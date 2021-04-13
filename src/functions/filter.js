5663ec09af2be5bf46bda56bd9ba47e011ea07cd
Hurrell <cem.hurrell@gmail.com>
2021-04-13 17:36:14 +0100

    Initial Filter function

---
+++ src/functions/filter.js
@@ -0,0 +1,27 @@
+export default function (data, categories, tags, startDate, endDate, top10) {
+  let filteredData = {};
+  console.log("categories", categories);
+  console.log("dataintofilter", data);
+  filteredData["Fee-Orders"] = data["Fee-Orders"].filter((item) => {
+    // if (!(item.tag in tags)) {
+    //   return false;
+    // }
+    if (!categories.includes(item.category)) {
+      return false;
+    }
+    return true;
+  });
+
+  filteredData["Fee-Earnings"] = data["Fee-Earnings"].filter((item) => {
+    // if (!(item.trackingId in tags)) {
+    //   return false;
+    // }
+    if (!categories.includes(item.category)) {
+      return false;
+    }
+    return true;
+  });
+  console.log("datafiltered", filteredData);
+
+  return filteredData;
+}
